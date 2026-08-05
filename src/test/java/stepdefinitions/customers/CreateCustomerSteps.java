23b0992f06275b4b539bc37c339ff9aa06b750cf
AnaJ123hib <anajcrus@gmail.com>
2026-08-05 16:01:33 -0500

    Updating Create customer feature tests

--- src/test/java/stepdefinitions/customers/CreateCustomerSteps.java
+++ src/test/java/stepdefinitions/customers/CreateCustomerSteps.java
@@ -4,12 +4,14 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.customers.CreateCustomerPage;
+import pages.customers.CustomerDetailsPage;
 import utils.CustomerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CreateCustomerSteps {
     CreateCustomerPage createCustomerPage;
+    CustomerDetailsPage customerDetailsPage;
     @And("the user is on the create customer page")
     public void theUserIsOnTheCreateCustomerPage() {
         createCustomerPage.navigateToMembersPage();
@@ -19,25 +21,14 @@ public class CreateCustomerSteps {
     @When("the user creates a new customer with valid information")
     public void theUserCreatesANewCustomerWithValidInformation() {
         createCustomerPage.fillCustomerForm(CustomerFactory.DEFAULT_CUSTOMER);
-        createCustomerPage.createCustomer();
+        createCustomerPage.saveCustomerData();
     }
 
     @Then("the customer details page should be displayed")
     public void theCustomerDetailsPageShouldBeDisplayed() {
-        assertThat(createCustomerPage.customerUsernameIsDisplayed(CustomerFactory.DEFAULT_CUSTOMER)).isTrue();
+        assertThat(customerDetailsPage.customerUsernameIsDisplayed(CustomerFactory.DEFAULT_CUSTOMER)).isTrue();
     }
 
-    @When("the user attempts to create a customer without name")
-    public void theUserAttemptsToCreateACustomerWithoutName() {
-        createCustomerPage.fillCustomerForm(CustomerFactory.CUSTOMER_WITHOUT_NAME);
-        createCustomerPage.createCustomer();
-    }
-
-    @When("the user attempts to create a customer without customer type")
-    public void theUserAttemptsToCreateACustomerWithoutCustomerType() {
-        createCustomerPage.fillCustomerForm(CustomerFactory.CUSTOMER_WITHOUT_TYPE);
-        createCustomerPage.createCustomer();
-    }
 
     @Then("a validation error message should be displayed")
     public void aValidationErrorMessageShouldBeDisplayed() {
@@ -56,6 +47,12 @@ public class CreateCustomerSteps {
 
     @Then("the customer should not be created")
     public void theCustomerShouldNotBeCreated() {
-        assertThat(createCustomerPage.isCustomerDisplayed(CustomerFactory.RANDOM_CUSTOMER)).isFalse();
+        assertThat(customerDetailsPage.isCustomerDisplayed(CustomerFactory.RANDOM_CUSTOMER)).isFalse();
+    }
+
+    @When("the user attempts to create a customer without {string}")
+    public void theUserAttemptsToCreateACustomerWithout(String field) {
+        createCustomerPage.fillCustomerForm(CustomerFactory.getCustomerWithoutField(field));
+        createCustomerPage.saveCustomerData();
     }
 }
