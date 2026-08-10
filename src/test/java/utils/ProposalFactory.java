e7f1e223db75e89c949eda229d6e606121634e87
AnaJ123hib <anajcrus@gmail.com>
2026-08-10 13:54:00 -0500

    Update Proposal feature data

--- src/test/java/utils/ProposalFactory.java
+++ src/test/java/utils/ProposalFactory.java
@@ -6,8 +6,8 @@ public class ProposalFactory {
     public static ProposalData DEFAULT_PROPOSAL = ProposalData
             .builder()
             .vendedorIndex(1)
-            .paymentTerms("30 días")
-            .paymentMethod("Efectivo")
+            .paymentTerms("30 days")
+            .paymentMethod("Cash")
             .deliveryDate("10/10/2026")
             .build();
 }
